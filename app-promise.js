97beb85ebba311b7eeb79679ddad15ae84f96413
Jinxing-Li <jasonli@Jasons-MacBook-Pro.local>
2020-06-03 16:31:19 -0400

    finished

--- app-promise.js
+++ app-promise.js
@@ -1,4 +1,6 @@
-const yargs = require('yargs')
+const yargs = require('yargs');
+const axios = require('axios');
+
 
 const argv = yargs
      .option({
@@ -12,5 +14,31 @@ const argv = yargs
      .help()
      .alias('help', 'h')
      .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA8_lRgqJ17uUzeUttzFSgpoaOZeXmKjaY`
+
+axios.get(geocodeUrl).then((response) => {
+     if (response.data.status == 'ZERO_RESULTS') {
+          throw new Error('Unable to find that address.');
+     }
+     var lon = response.data.results[0].geometry.location.lng;
+     var lat = response.data.results[0].geometry.location.lat;
+     var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2e2cc437e883a6ce3490efaf6499ad35`;  
+     console.log(response.data.results[0].formatted_address)
+     return axios.get(weatherURL);
+}).then((response) => {
+     var temperature = response.data.main.temp;
+     var feels_like = response.data.main.feels_like;
+     console.log(`It's currently ${(temperature - 273.15).toFixed(2)}. It feels like ${(feels_like - 273.15).toFixed(2)}.`);
+}).catch((e) => {
+     if (e.code === 'ETIMEDOUT') {
+          console.log('Unable to connect to API servers.');
+     }
+     else {
+          console.log(e.message)
+     }
+})
+
 
 
